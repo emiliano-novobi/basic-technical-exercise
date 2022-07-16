744365d0a7eaf7b6a3d77428cf72bad2055a9419
Emiliano Menéndez <emystein@gmail.com>
2022-07-16 16:25:01 -0300

    Add total books to Book Location.

--- customized_addons/novobi_emi_library/models/models.py
+++ customized_addons/novobi_emi_library/models/models.py
@@ -45,7 +45,11 @@ class BookLocation(models.Model):
     name = fields.Char('Name')
     books = fields.One2many('library.book', 'location', string="Books")
     total_available_books = fields.Integer()
+    total_books = fields.Integer(compute='_compute_total_books')
 
+    @api.depends('books')
+    def _compute_total_books(self):
+        self.total_books = len(self.books)
 
 class BookLease(models.TransientModel):
     _name = "library.book_lease"
